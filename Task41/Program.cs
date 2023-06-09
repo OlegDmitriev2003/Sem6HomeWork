6734ece97b792ab5ebb5faa277cf03197cbb75e5
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-09 08:50:30 +0300

    Completed Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int Count = 0;
+Console.Write("Введите колличество чисел: ");
+int i = int.Parse(Console.ReadLine()!);
+for (int n = 0; n < i; n++)
+    {
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    if (num > 0)
+        Count++;
+    }
+Console.WriteLine($"Было введено {Count} чисел больше 0 ");
